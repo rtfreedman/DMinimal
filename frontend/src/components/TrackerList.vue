63d5e12e5c12421c19df75a11e0f2afb1cf07c18
rtfreedman <rtfreedman@gmail.com>
2018-09-19 20:42:39 -0400

    longrestall and tabs

--- frontend/src/components/TrackerList.vue
+++ frontend/src/components/TrackerList.vue
@@ -1,16 +1,22 @@
 <template>
-  <v-card>
-    <v-card-actions>
-      <v-btn flat @click="addCharacter()"> +Character </v-btn>
-      <v-btn v-if="characters.length > 1" flat color="blue">Long Rest All</v-btn>
-    </v-card-actions>
-    <div v-for="c in characters" :key="c">
-      <p class="text-lg-right">
-        <v-btn v-if="characters.length > 1" @click='removeCharacter(c)' icon flat color="red"> <v-icon>cancel</v-icon> </v-btn>
-      </p>
-      <tracker v-bind:classOpts='classOpts'></tracker>
-    </div>
-  </v-card>
+  <v-container>
+    <v-btn flat @click="addCharacter()"> +Character </v-btn>
+    <v-btn @click="longRestAll()" v-if="characters.length > 1" flat color="blue">Long Rest All</v-btn>
+    <v-tabs>
+      <v-tab v-for="c in characters" :key="c">
+            asdf
+            <v-btn v-if="characters.length > 1" @click='removeCharacter(c)' icon flat color="grey"> <v-icon>cancel</v-icon> </v-btn>
+      </v-tab>
+      <v-tabs-slider color="yellow"></v-tabs-slider>
+      <v-tabs-items>
+        <v-tab-item v-for="c in characters" :key="c">
+          <v-card flat>
+            <tracker :ref="'character' + c" v-bind:classOpts='classOpts'></tracker>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-tabs>
+  </v-container>
 </template>
 <script>
 import Tracker from '@/components/Tracker'
@@ -28,6 +34,12 @@ export default {
     Tracker
   },
   methods: {
+    longRestAll () {
+      for (let i in this.characters) {
+        // if someone could explain to me why the ['0'] is needed that would be awesome
+        this.$refs['character' + this.characters[i]]['0'].longRest()
+      }
+    },
     addCharacter () {
       if (this.characters.length < 10) {
         this.characters.push(Math.random() * (10 ** 10))
