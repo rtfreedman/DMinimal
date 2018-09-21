1e0dd89c5929b2abe0efb81e23cb30cdab43cc25
rtfreedman <rtfreedman@gmail.com>
2018-09-20 21:59:58 -0400

    fixed bug with flags not being parsed

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ var external bool
 func main() {
 	flag.IntVar(&port, "port", 8010, "Port to run API on")
 	flag.BoolVar(&external, "external", false, "Run as externally accessible API")
+	flag.Parse()
 	api.RunAPI(port, external)
 }
