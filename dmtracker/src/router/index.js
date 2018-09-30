7dbabbaee0fbc7c9fd2997d84f15437199ef0747
rtfreedman <rtfreedman@gmail.com>
2018-09-30 18:19:46 -0400

    switched names

--- dmtracker/src/router/index.js
+++ dmtracker/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Tracker from '@/components/Tracker'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'DMTracker',
+      component: Tracker
     }
   ]
 })
