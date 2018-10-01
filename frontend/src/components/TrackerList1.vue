82f06f0779f0c33a006bd3556fb10ec8fc8a0c4d
rtfreedman <rtfreedman@gmail.com>
2018-09-30 21:23:41 -0400

    trackerlist looking pretty good so far

--- frontend/src/components/TrackerList1.vue
+++ frontend/src/components/TrackerList1.vue
@@ -9,6 +9,17 @@
         <v-btn v-if="characters.length > 1 && c.id !== 0" @click='removeCharacter(c)' icon flat color="grey"> <v-icon>cancel</v-icon> </v-btn>
       </v-tab>
     </v-tabs>
+    <v-tabs-items v-model="tabs">
+      <v-tab-item v-for="c in characters" :key="c.id">
+        <v-card flat>
+          <v-card-text>
+            <v-flex xs6>
+              <v-text-field v-model="c.name" placeholder="Name..."></v-text-field>
+            </v-flex>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
