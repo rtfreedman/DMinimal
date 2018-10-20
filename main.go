5143b589108e9c3837be609077a6cc39ba13a487
rtfreedman <rtfreedman@gmail.com>
2018-10-19 20:03:28 -0400

    backend appears to be integrated with postgres. goint ot do mroe testing later

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 
+	"github.com/rtfreedman/DMinimal/routines"
 	"github.com/rtfreedman/SpellTracker/api"
 )
 
@@ -13,5 +14,6 @@ func main() {
 	flag.IntVar(&port, "port", 8010, "Port to run API on")
 	flag.BoolVar(&external, "external", false, "Run as externally accessible API")
 	flag.Parse()
+	routines.Setup()
 	api.RunAPI(port, external)
 }
