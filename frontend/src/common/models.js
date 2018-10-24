fb89b552c3b9bc5f90be2a10bcc2b029f8a8372e
Daren McCulley <daren.mcculley@cerberus.cira>
2018-10-23 23:37:23 -0400

    use classes, move mutations

---
+++ frontend/src/common/models.js
@@ -0,0 +1,52 @@
+export class Class {
+  constructor() {
+    this.classname = 'Bard'
+    this.level = 1
+    this.slots = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+    }
+    this.workingSlots = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+    }
+  }
+}
+
+export class Character {
+  constructor() {
+    this.id = 1
+    this.deathThrows = 0
+    this.lifeThrows = 0
+    this.initiative = null
+    this.hitpoints = 1
+    this.maxHitpoints = 1
+    this.name = ''
+    this.proficiency = 0
+    this.rollHealth = true
+    this.concentrating = ''
+    this.classes = [new Class()]
+    this.abilityScores = {
+      STR: 10,
+      INT: 10,
+      WIS: 10,
+      DEX: 10,
+      CON: 10,
+      CHR: 10,
+    }
+  }
+}
