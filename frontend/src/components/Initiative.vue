86106f2344d59feac5e9ed7cd986c43d9ed8a8c5
Daren McCulley <daren.mcculley@cerberus.cira>
2018-10-27 15:36:14 -0400

    init bug

--- frontend/src/components/Initiative.vue
+++ frontend/src/components/Initiative.vue
@@ -1,26 +1,16 @@
 <template>
   <v-layout>
-    <!-- <v-btn
-      color="primary"
-      flat
-      icon
-      @click="rollInitative"
-    >
-      <v-icon>mdi-dice-multiple</v-icon>
-    </v-btn>-->
-    {{ character.initiative }}
     <v-text-field
       v-model="character.initiative"
       clearable
       outline
-      :rules="[mustBeNum, minNum]"
       class="ml-2"
       append-icon="mdi-dice-multiple"
       :append-icon-cb="rollInitiative"
       label="Initiative"
       style="max-width: 170px"
       hide-details
-    >{{ character.initiative || '?' }}</v-text-field>
+    >{{ character.initiative > 0 ? character.initiative : null }}</v-text-field>
     <!-- Conditional clear button -->
   </v-layout>
 </template>
@@ -37,22 +27,6 @@ export default {
         Math.floor(Math.random() * 20) +
         1
       this.character.initiative = parseInt(value)
-    },
-
-    mustBeNum(val) {
-      if (typeof val === 'string' && val.toLowerCase().includes('e')) {
-        return 'Scientific notation not allowed'
-      }
-      if (isNaN(parseInt(val))) {
-        return 'Input is not a number'
-      }
-      return true
-    },
-    minNum(val) {
-      if (parseInt(val) < 0) {
-        return 'No negative numbers'
-      }
-      return true
     },
   },
 }
