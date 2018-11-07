608c69f7d7f0db3a5c538943ede2f4c658e46223
rtfreedman <rtfreedman@gmail.com>
2018-11-06 20:34:46 -0500

    starting monsters... this is going to be a big one

--- frontend/src/api/monsters.js
+++ frontend/src/api/monsters.js
@@ -4,7 +4,6 @@ export default {
   getMonsters() {
     return axios.get('/api/monsters').then(response => response.data)
   },
-
   getMonster(monster) {
     return axios.get('/api/monsters/' + monster).then(response => response.data)
   },
