a8d31e3df43147539608caf762ccf116b8f41340
rtfreedman <rtfreedman@gmail.com>
2018-11-30 12:52:36 -0500

    refactoring ingest

--- mongo_data/ingest.py
+++ mongo_data/ingest.py
@@ -2,18 +2,26 @@ from pymongo import MongoClient
 from bson import json_util
 # import getpass
 
-# mongodb = MongoClient("mongodb://wizerd:{}@127.0.0.1:27041".format(getpass.getpass(prompt="Mongo Password: ")))
 mongodb = MongoClient("mongodb://127.0.0.1:27041")
-with open('mongo_data/monsters.json') as f:
-    content = json_util.loads(f.read())
+# mongodb = MongoClient("mongodb://wizerd:{}@127.0.0.1:27041".format(getpass.getpass(prompt="Mongo Password: ")))
+
+
+def import_mongo_data(db, filename):
+    with open(filename) as f:
+        content = json_util.loads(f.read())
+    if type(content) is not dict or len(content.keys()) == 0:
+        raise Exception("Content error: json data must be shaped like : {'collectionname':[documents...], 'othercollectionname': [otherdocuments...], ...}")
+    for key in content.keys():
+        if type(content[key]) is not list:
+            raise Exception("Content error: json data must be shaped like : {'collectionname':[documents...]}")
+    for collection_name in content.keys():
+        collection_name = list(content.keys())[0]
+        collection = db[collection_name]
+        documents = content[collection_name]
+        collection.insert_many(documents)
+
+
 dnd = mongodb.dnddb
-monsters_collection = dnd.monsters
-monsters_collection.insert_many(content["monsters"])
-with open('mongo_data/items.json') as f:
-    content = json_util.loads(f.read())
-items_collection = dnd.items
-items_collection.insert_many(content["items"])
-with open("mongo_data/spells.json") as f:
-    content = json_util.loads(f.read())
-spells_collection = dnd.spells
-spells_collection.insert_many(content["spells"])
+import_mongo_data(dnd, 'mongo_data/monsters.json')
+import_mongo_data(dnd, 'mongo_data/items.json')
+import_mongo_data(dnd, 'mongo_data/spells.json')
