bebc507d5030970d7a1d20abb932947242991135
rtfreedman <rtfreedman@gmail.com>
2018-11-30 12:40:02 -0500

    updating data

--- mongo_data/ingest.py
+++ mongo_data/ingest.py
@@ -4,12 +4,16 @@ from bson import json_util
 
 # mongodb = MongoClient("mongodb://wizerd:{}@127.0.0.1:27041".format(getpass.getpass(prompt="Mongo Password: ")))
 mongodb = MongoClient("mongodb://127.0.0.1:27041")
-with open('mongo_data/monster_data_full.json') as f:
+with open('mongo_data/monsters.json') as f:
     content = json_util.loads(f.read())
 dnd = mongodb.dnddb
 monsters_collection = dnd.monsters
 monsters_collection.insert_many(content["monsters"])
-with open('mongo_data/item_data.json') as f:
+with open('mongo_data/items.json') as f:
     content = json_util.loads(f.read())
 items_collection = dnd.items
 items_collection.insert_many(content["items"])
+with open("mongo_data/spells.json") as f:
+    content = json_util.loads(f.read())
+spells_collection = dnd.spells
+spells_collection.insert_many(content["spells"])
