41e444bac52c47245d03d5c61e2580b52daa0901
rtfreedman <rtfreedman@gmail.com>
2018-11-30 12:34:47 -0500

    item_data

--- mongo_data/ingest.py
+++ mongo_data/ingest.py
@@ -9,3 +9,7 @@ with open('mongo_data/monster_data_full.json') as f:
 dnd = mongodb.dnddb
 monsters_collection = dnd.monsters
 monsters_collection.insert_many(content["monsters"])
+with open('mongo_data/item_data.json') as f:
+    content = json_util.loads(f.read())
+items_collection = dnd.items
+items_collection.insert_many(content["items"])
